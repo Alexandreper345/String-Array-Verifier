bad2ccafd679c03fcca2b23a74b4a07bd5f03077
Alexandre da Silva Peroba <138054329+Alexandreper345@users.noreply.github.com>
2024-04-08 14:01:48 -0300

    Update main.rs

--- par/src/main.rs
+++ par/src/main.rs
@@ -2,26 +2,15 @@ use std::collections::HashMap;
 
 fn is_vetor_par(value: [i8; 5]) -> HashMap<i8,u8>{
     
-
-
-    //aqui eu crio um contador para que ele venha contar cada elemento da minha array permitindo
-    //uma assinatura de  tipo explicita como o "i8 e u8" para que eles alocar numeros positivos e
-    // negativos e criando uma instacia para chaves e valores     
+    //aqui eu crio um contador para que ele venha contar cada elemento da minha array,colocando chave e valor como o "i8 e u8" para que eles alocar numeros positivos e
+    // negativos e criando uma instacia para chaves e valores   
     let mut contador: HashMap<i8,u8> = HashMap::new();
     
-
-
-
-    //faço um for para interar com o valor da minha lista e dá a entrada do meu contador mutavel
-    //dentro do valor da minha lista para que
-    //ele venha passando de um em um de cada elemento do meu valor
+    //iteramos sobre os elementos
     for num in value.iter(){
         *contador.entry(*num).or_insert(0)+=1;
 
     }
-    //aqui eu criou uma tupla pra que assim o valor no  "_" que eu passar como exemplo o numero
-    //alocado na memoria da lista e quantas vezes ele se repete, iterando tudo isso dentro do
-    //contador mutavel.
     for (_,repeticoes) in contador.iter_mut(){
         if *repeticoes > 0{
            *repeticoes /= 2
